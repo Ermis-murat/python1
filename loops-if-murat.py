619b5f534a24ebd244a478b0ef1e0b698c54a16e
Barley <Murat.ermis.d15@gmail.com>
2025-05-28 10:06:21 +0200

    loops-if

---
+++ loops-if-murat.py
@@ -0,0 +1,12 @@
+count = 1
+while count <= 10: #loop tot 10
+    print(count)
+    count += 1 # volgende getal
+
+count = 12
+while count <= 36: #loop tot 36
+    if count >= 18: #if statment
+        print( "Je bent", count, " zorg ervoor dat je een zorgverzekering hebt afgesloten"), #print
+    else: print( count, "Je hoeft nog geen zorgverzekering af te sluiten")
+
+    count += 1
